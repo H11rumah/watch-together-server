fc6784d209397ee2f84c229c931c89a0010c3b85
H11rumah <grebenkin.sergei030800@gmail.com>
2023-07-14 14:47:21 +0500

    commit

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const app = express();
 const WSServer = require("express-ws")(app);
 const aWss = WSServer.getWss();
 
-const http = require("https");
+const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
 const server = http
     .createServer((req, res) => {
         console.log("server request");
+
+        res.write("abc");
+        res.end();
     })
     .listen(PORT, "watch-togerther-server.onrender.com", (error) => {
         error ? console.log(error) : console.log("listening port ", PORT);
