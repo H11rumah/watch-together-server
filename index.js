8a0f820d4b6a84b2a0200e6a1a796b8337bc8f63
H11rumah <grebenkin.sergei030800@gmail.com>
2023-07-14 14:27:30 +0500

    commit

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = express();
 const WSServer = require("express-ws")(app);
 const aWss = WSServer.getWss();
 
+const http = require("http");
+
 const PORT = process.env.PORT || 5000;
+
+const server = http
+    .createServer((req, res) => {
+        console.log("server request");
+    })
+    .listen(PORT, "localhost", (error) => {
+        error ? console.log(error) : console.log("listening port ", PORT);
+    });
 
 let rooms = {
     // example: {
